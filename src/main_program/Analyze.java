193d3a0540a25f9e92836c8cfac7992959553672
CAULLIREAU Dorian <54810120+caullird@users.noreply.github.com>
2021-11-25 23:52:10 +0100

    Update Analyze.java

--- src/main_program/Analyze.java
+++ src/main_program/Analyze.java
@@ -4,19 +4,32 @@ import java.util.List;
 import config.ReduceFile;
 
 public class Analyze {
+	
+	/* Constructor of the File Analyze
+	 * 
+	 * Manage all the programm
+	 * 
+	 */
 
 	public Analyze(){
 		
+		
+		// Allows the user to choose the maximum number of threads
 		int nbThreadMax = 7;
 		
+		// Allows the user to choose the file to be processed
 		String freqFile = "big.txt";
 		
+		// Allows the reading of the file 
 		ReduceFile stringBook = new ReduceFile(freqFile, nbThreadMax);
 	
+		// Allows to calculate the optimal number of threads
 		int nbThread = stringBook.getNbThread();
 		
+		// Allows to generate maps for multi thread management
 		List<List<String>> maps = new MapGenerator(stringBook,nbThread).GenerateMaps();
 		
+		// Creation of treads for the reduce
 		new ThreadGenerator(maps,nbThread).generateThread();
 
 	}
