0bca15b34db61fd6ef4c3e4d061273e24774b4db
CAULLIREAU Dorian <54810120+caullird@users.noreply.github.com>
2021-11-26 00:10:10 +0100

    Update ThreadGenerator.java

--- src/main_program/ThreadGenerator.java
+++ src/main_program/ThreadGenerator.java
@@ -10,6 +10,12 @@ public class ThreadGenerator {
 	
 	private int nbThread;
 	
+        /* Constructor of the ThreadGenerator Class
+	 * 
+	 * Create ThreadGenerator Object 
+	 * 
+	 */
+	
 	public ThreadGenerator(List<List<String>> maps, int nbThread) {
 		this.setMaps(maps);
 		this.reducer = new Reducer(nbThread);
@@ -17,28 +23,45 @@ public class ThreadGenerator {
 	}
 	
 	public ThreadGenerator generateThread(){
+		
+		/* generateThread() function
+		 * 
+		 * Allows the generation of threads according to the different information
+		 * 
+		 */
+		
+		
 		for(List<String> map : this.getMaps()) {
 			new Thread(new ThreadMapManagement(map, this.reducer, this.getNbThread())).start();
 		}
 		
 		return null;
 	}
+	
+		
+	/* Getters & Setters
+	 * 
+	 * 
+	 * Function allowing the modification and recovery of the elements of the object
+	 * 
+    	 * Getters 
+    	 * ----------
+     	 *  - getMaps() : is used to retrieve all the maps
+	 *  - getNbThread() : is used to retrieve the number of threads
+	 *  
+	 * Setters 
+	 * ----------
+	 *  - setMaps() : is used to modify the maps
+	 *  - setNbThread() : is used to modify the number if threads
+	 * 
+	 */
 
-	public List<List<String>> getMaps() {
-		return maps;
-	}
+	public List<List<String>> getMaps() { return maps; }
 
-	public void setMaps(List<List<String>> maps) {
-		this.maps = maps;
-	}
+	public void setMaps(List<List<String>> maps) { this.maps = maps; }
 
-	public int getNbThread() {
-		return nbThread;
-	}
+	public int getNbThread() { return nbThread; }
 
-	public void setNbThread(int nbThread) {
-		this.nbThread = nbThread;
-	}
-
+	public void setNbThread(int nbThread) { this.nbThread = nbThread; }
 	
 }
